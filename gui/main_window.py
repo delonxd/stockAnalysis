db183995ca46e10cd8ea7b47ed7e57685ce6a46f
李继隆 <delong1q2w3e@163.com>
2021-09-30 16:55:02 +0800

    修改 main_window.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -10,7 +10,7 @@ from PyQt5.QtGui import *
 import sys
 
 
-def sql2df(columns):
+def sql2df():
     config = {
         'user': 'root',
         'password': 'aQLZciNTq4sx',
@@ -24,16 +24,68 @@ def sql2df(columns):
     sql_df = get_data_frame(cursor=cursor, table='bs_600008')
 
     sql_df = sql_df.set_index('standardDate', drop=False)
-    sql_df = sql_df.loc[:, columns]
-    sql_df = sql_df.where(sql_df.notnull(), None)
+    # sql_df = sql_df.loc[:, columns]
+    # sql_df = sql_df.where(sql_df.notnull(), None)
     return sql_df
+
+def get_data_source():
+    df = sql2df()
+
+
+
+class DataSource:
+    def __init__(self, name, df):
+        self.name = name
+        self.df = df
+
+        self.color = Qt.red
+        self.line_thick = 1
+        self.pen_type = Qt.SolidLine
+
+        self.scale_max = 1024 * 1e8
+        self.scale_min = 1e8
+
+        self.scale_div = 10
+        self.logarithmic = True
+
+        self.val_max = None
+        self.val_min = None
+        self.val_delta = None
+        self.metrics = None
+
+        self.format = None
+
+        self.set_val_scale()
+
+    def set_val_scale(self):
+        if self.logarithmic is True:
+            self.val_max = 1
+            self.val_min = 0
+        else:
+            self.val_max = self.scale_max
+            self.val_min = self.scale_min
+
+        self.val_delta = self.val_max - self.val_min
+        self.metrics = list(np.linspace(self.val_min, self.val_max, self.scale_div+1))
+        self.metrics = self.metrics[1:-1]
+
+    def get_y_metrics(self):
+        pass
 
 
 class DataPix:
     def __init__(self):
         # get df
-        self.df = sql2df(['id_115'])
-        self.df.sort_index(inplace=True)
+        # self.df = sql2df(['id_115'])
+        # self.df.sort_index(inplace=True)
+        #
+        # data_source = DataSource('所有者权益', self.df)
+        # data_source.format = lambda x: '%i亿' % (x / 1e8)
+
+        self.data_dict = dict()
+        # self.data_dict[data_source.name] = data_source
+
+        self.default_ds = data_dict['所有者权益']
 
         # date metrics
         self.date_max = dt.date(2022, 7, 20)
@@ -42,18 +94,7 @@ class DataPix:
 
         years = list(range(self.date_min.year+1, self.date_max.year+1, 1))
         self.date_metrics = [dt.date(x, 1, 1) for x in years]
-
         self.y_value_metrics = list(range(1, 10))
-
-        # data metrics
-        self.data_max = (2 ** 10) * 1e8
-        self.data_min = 1e8
-
-        _, self.val_max = self.data2value(None, self.data_max)
-        _, self.val_min = self.data2value(None, self.data_min)
-        self.d_val = self.val_max - self.val_min
-
-        # self.data_metrics = []
 
         # structure
         d_width = 800
@@ -72,8 +113,8 @@ class DataPix:
 
         # draw
         self.draw_struct()
-        self.draw_metrics()
-        self.draw_data()
+        self.draw_metrics(self.default_ds)
+        self.draw_data(self.data_dict['gdqy'])
 
     def draw_struct(self):
         pix_painter = QPainter(self.pix)
@@ -95,11 +136,11 @@ class DataPix:
 
         pix_painter.end()
 
-    def draw_metrics(self):
-        self.draw_x_metrics()
-        self.draw_y_metrics()
+    def draw_metrics(self, data: DataSource):
+        self.draw_x_metrics(data)
+        self.draw_y_metrics(data)
 
-    def draw_x_metrics(self):
+    def draw_x_metrics(self, data: DataSource):
         pix_painter = QPainter(self.pix)
 
         pix_painter.setFont(QFont('Consolas', 10))
@@ -110,7 +151,7 @@ class DataPix:
         d_bottom = self.data_rect.bottom() + 1
 
         for date in self.date_metrics:
-            x, _ = self.data2px(date, None)
+            x, _ = self.data2px(date, None, data)
             txt = str(date.year)
 
             width = pix_painter.fontMetrics().width(txt) + 2
@@ -127,7 +168,7 @@ class DataPix:
 
         pix_painter.end()
 
-    def draw_y_metrics(self):
+    def draw_y_metrics(self, data: DataSource):
         pix_painter = QPainter(self.pix)
 
         pix_painter.setFont(QFont('Consolas', 10))
@@ -137,10 +178,10 @@ class DataPix:
         d_left = self.data_rect.left()
         d_right = self.data_rect.right()
         
-        for value in self.y_value_metrics:
-            _, y = self.value2px(None, value)
-            _, data = self.value2data(None, value)
-            txt = '%i亿' % (data / 1e8)
+        for val_y in data.metrics:
+            _, y = self.value2px(None, val_y, data)
+            _, val_src = self.value2data(None, val_y, data)
+            txt = data.format(val_src)
 
             width = pix_painter.fontMetrics().width(txt) + 2
             height = pix_painter.fontMetrics().height() + 2
@@ -156,65 +197,70 @@ class DataPix:
 
         pix_painter.end()
 
-    def draw_data(self):
+    def draw_data(self, data: DataSource):
         pix_painter = QPainter(self.pix)
 
         pen = QPen(Qt.red, 2, Qt.SolidLine)
         pix_painter.setPen(pen)
 
         point1 = None
-        point2 = None
-        for tup in self.df.itertuples():
+        for tup in data.df.itertuples():
             if tup[1]:
                 date = dt.datetime.strptime(tup[0], "%Y-%m-%dT00:00:00+08:00").date()
                 value = tup[1]
-                x, y = self.data2px(date, value)
+                x, y = self.data2px(date, value, data)
                 point2 = QPoint(x, y)
                 if point1:
                     pix_painter.drawLine(point1, point2)
                 point1 = point2
         pix_painter.end()
 
-    def data2value(self, date, value):
+    def data2value(self, date, value, data: DataSource):
         x, y = None, None
         if date is not None:
             x = (date - self.date_min).days
         if value is not None:
-            y = np.log2(value / 1e8)
+            if data.logarithmic is True:
+                y = np.log2(value / data.scale_min) / np.log2(data.scale_max / data.scale_min)
+            else:
+                y = value
         return x, y
 
-    def value2data(self, x, y):
+    def value2data(self, x, y, data: DataSource):
         date, value = None, None
         if x is not None:
             date = self.date_min + dt.timedelta(days=x)
         if y is not None:
-            value = (2 ** y) * 1e8
+            if data.logarithmic is True:
+                value = 2 ** (y * np.log2(data.scale_max / data.scale_min)) * data.scale_min
+            else:
+                value = y
         return date, value
 
-    def value2px(self, val_x, val_y):
+    def value2px(self, val_x, val_y, data: DataSource):
         x, y = None, None
         if val_x is not None:
             x = self.data_rect.x() + val_x * (self.data_rect.width()-1) / self.d_date
         if val_y is not None:
-            y = (self.val_max - val_y) * self.data_rect.height() / self.d_val
+            y = (data.val_max - val_y) * self.data_rect.height() / data.val_delta
         return x, y
 
-    def px2value(self, px_x, px_y):
+    def px2value(self, px_x, px_y, data: DataSource):
         x, y = None, None
         if px_x is not None:
             x = (px_x - self.data_rect.x()) * self.d_date / (self.data_rect.width() - 1)
         if px_y is not None:
-            y = self.val_max - px_y * self.d_val / self.data_rect.height()
+            y = data.val_max - px_y * data.val_delta / self.data_rect.height()
         return x, y
 
-    def data2px(self, date, value):
-        val_x, val_y = self.data2value(date, value)
-        x, y = self.value2px(val_x, val_y)
+    def data2px(self, date, value, data):
+        val_x, val_y = self.data2value(date, value, data)
+        x, y = self.value2px(val_x, val_y, data)
         return x, y
 
-    def px2data(self, px_x, px_y):
-        val_x, val_y = self.px2value(px_x, px_y)
-        date, value = self.value2data(val_x, val_y)
+    def px2data(self, px_x, px_y, data):
+        val_x, val_y = self.px2value(px_x, px_y, data)
+        date, value = self.value2data(val_x, val_y, data)
         return date, value
 
 
@@ -225,13 +271,15 @@ class MainWindow(QWidget):
         self.setWindowTitle('绘制图形')
 
         self.label = QLabel(self)
+        self.label2 = QLabel(self)
+        pix = QPixmap(200, 600)
+        pix.fill(Qt.black)
+        self.label2.setPixmap(pix)
 
-        layout = QVBoxLayout()
+        layout = QHBoxLayout()
         layout.addWidget(self.label, 0, Qt.AlignCenter)
+        layout.addWidget(self.label2, 0, Qt.AlignCenter)
         self.setLayout(layout)
-
-        # self.df = sql2df(['id_115'])
-        # self.df.sort_index(inplace=True)
 
         self.data_pix = DataPix()
 
@@ -276,12 +324,11 @@ class MainWindow(QWidget):
         self.painter.end()
         self.pix_show = pix
 
-        date, val = self.data_pix.px2data(x, y)
-
+        date, val = self.data_pix.px2data(x, y, self.data_pix.default_ds)
         date_str = date.strftime("%Y-%m-%d")
-        self.draw_tooltip(x, self.data_pix.data_rect.bottom() + 1, date_str)
+        val_str = self.data_pix.default_ds.format(val)
 
-        val_str = '%.2e' % val
+        self.draw_tooltip(x, self.data_pix.data_rect.bottom() + 1, date_str)
         self.draw_tooltip(self.data_pix.data_rect.right() + 1, y, val_str)
 
         self.update()
