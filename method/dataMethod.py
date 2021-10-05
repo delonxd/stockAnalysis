0fe8b30bdb588862742aeefb68d224f0fe3deaba
Delon <delong1q2w3e@163.com>
2021-10-05 16:10:02 +0800

    修改 dataMethod.py

--- method/dataMethod.py
+++ method/dataMethod.py
@@ -11,9 +11,11 @@ from dateutil.rrule import *
 import pandas as pd
 import numpy as np
 import sys
+import pickle
+import json
 
 
-def sql2df():
+def sql2df(code):
     config = {
         'user': 'root',
         'password': 'aQLZciNTq4sx',
@@ -24,8 +26,10 @@ def sql2df():
     }
     db = mysql.connector.connect(**config)
 
+    table = 'fs_' + code
+
     cursor = db.cursor()
-    sql_df = get_data_frame(cursor=cursor, table='fs_600008')
+    sql_df = get_data_frame(cursor=cursor, table=table)
 
     sql_df = sql_df.set_index('standardDate', drop=False)
 
@@ -91,6 +95,9 @@ def get_tree_df2():
 
     df = transpose_df(header_df)
 
+    df.insert(0, "ds_type", 'digit')
+    df.insert(0, "child", None)
+
     df.insert(0, "index_name", df.index)
     df.insert(1, "selected", False)
 
@@ -98,15 +105,17 @@ def get_tree_df2():
     df.insert(3, "line_thick", 2)
     df.insert(4, "pen_style", Qt.SolidLine)
 
-    df.insert(5, "scale_min", 2e8)
-    df.insert(6, "scale_max", 2048*1e8)
+    df.insert(5, "scale_min", 1)
+    df.insert(6, "scale_max", 1024)
     df.insert(7, "scale_div", 10)
     df.insert(8, "logarithmic", True)
-    df.insert(9, "format_fun", lambda x: '%i亿' % (x / 1e8))
 
-    df.insert(10, "info_priority", 0)
+    df.insert(9, "info_priority", 0)
 
-    df.insert(2, "show_name", '')
+    df.insert(10, "units", '亿')
+
+    df.insert(0, "show_name", '')
+    df.insert(0, "default_ds", False)
 
     # show_df(df)
 
@@ -114,25 +123,58 @@ def get_tree_df2():
 
 
 def get_default_style_df():
-    df = get_tree_df2()
+    # df = get_tree_df2()
+    #
+    # df.loc['id_001_bs_ta', 'color'] = QColor(Qt.green)
+    # df.loc['id_001_bs_ta', 'selected'] = True
+    # df.loc['id_001_bs_ta', 'show_name'] = '资产合计'
+    # df.loc['id_001_bs_ta', 'info_priority'] = 3
+    # df.loc['id_001_bs_ta', 'default_ds'] = True
+    #
+    # df.loc['id_062_bs_tl', 'color'] = QColor(Qt.red)
+    # df.loc['id_062_bs_tl', 'selected'] = True
+    # df.loc['id_062_bs_tl', 'show_name'] = '负债合计'
+    # df.loc['id_062_bs_tl', 'info_priority'] = 2
+    #
+    # df.loc['id_110_bs_toe', 'color'] = QColor(Qt.yellow)
+    # df.loc['id_110_bs_toe', 'selected'] = True
+    # df.loc['id_110_bs_toe', 'show_name'] = '所有者权益'
+    # df.loc['id_110_bs_toe', 'info_priority'] = 1
+    #
+    # df.loc['first_update', 'ds_type'] = 'str'
+    # df.loc['last_update', 'ds_type'] = 'str'
+    # df.loc['stockCode', 'ds_type'] = 'const'
+    # df.loc['currency', 'ds_type'] = 'const'
+    # df.loc['standardDate', 'ds_type'] = 'str'
+    # df.loc['reportDate', 'ds_type'] = 'str'
+    # df.loc['reportType', 'ds_type'] = 'str'
+    # df.loc['date', 'ds_type'] = 'str'
+    #
+    # # show_df(df)
+    # # print(df['info_priority'].max())
+    # # print(df.quantile(0.5))
 
-    df.loc['id_001_bs_ta', 'color'] = QColor(Qt.green)
-    df.loc['id_001_bs_ta', 'selected'] = True
-    df.loc['id_001_bs_ta', 'show_name'] = '资产合计'
-    df.loc['id_001_bs_ta', 'info_priority'] = 3
-
-    df.loc['id_062_bs_tl', 'color'] = QColor(Qt.red)
-    df.loc['id_062_bs_tl', 'selected'] = True
-    df.loc['id_062_bs_tl', 'show_name'] = '负债合计'
-    df.loc['id_062_bs_tl', 'info_priority'] = 2
-
-    df.loc['id_110_bs_toe', 'color'] = QColor(Qt.yellow)
-    df.loc['id_110_bs_toe', 'selected'] = True
-    df.loc['id_110_bs_toe', 'show_name'] = '所有者权益'
-    df.loc['id_110_bs_toe', 'info_priority'] = 1
+    path = '../gui/style_df_standard.pkl'
+    with open(path, 'rb') as pk_f:
+        df = pickle.load(pk_f)
 
     return df
 
 
 if __name__ == '__main__':
+
+    path = '../basicData/BasicData.pkl'
+    with open(path, 'rb') as pk_f:
+        res = pickle.load(pk_f)
+
+    d = json.loads(res)['data']
+
+    str0 = ''
+    for v in d:
+        str0 = '\n'.join([str0, repr(v)])
+    print(str0)
+
+    with open("../comparisonTable/basic_table.txt", "w", encoding="utf-8") as f:
+        f.write(str0)
+
     pass
