d92a8bda151490042d7572a8811406e33d0d8b4e
李继隆 <delong1q2w3e@163.com>
2021-10-11 16:39:26 +0800

    添加 name_table.txt 添加 test_json.py

---
+++ test/test_json.py
@@ -0,0 +1,35 @@
+import json
+import pickle
+
+
+def test_json():
+
+    path = '../gui/style_combined_default0.pkl'
+    with open(path, 'rb') as pk_f:
+        df = pickle.load(pk_f)
+
+    tmp_str = ''
+    for index, row in df.iterrows():
+
+        print(index)
+        print(row.values)
+
+        row_str = ' | '.join([row['txt_CN'], row['index_name']])
+        tmp_str = '\n'.join([tmp_str, row_str])
+
+    # print(df)
+    # tmp = df.to_json(orient="columns", force_ascii=False)
+    tmp = df.to_json(orient="split", force_ascii=False)
+
+    res = json.dumps(tmp)
+
+    with open("../test/style_test.json", "w", encoding="utf-8") as f:
+        f.write(res)
+    pass
+
+    with open("../comparisonTable/name_table.txt", "w", encoding="utf-8") as f:
+        f.write(tmp_str)
+
+
+if __name__ == '__main__':
+    test_json()
