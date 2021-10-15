391122863995a70f58668b73ebe8a5a0d9e0927f
李继隆 <delong1q2w3e@163.com>
2021-10-15 13:40:26 +0800

    修改 requestBasicData.py

--- request/requestBasicData.py
+++ request/requestBasicData.py
@@ -3,16 +3,20 @@ import json
 import pickle
 
 
-url = 'https://open.lixinger.com/api/a/company'
+if __name__ == '__main__':
+    url = 'https://open.lixinger.com/api/a/company'
 
-data = {"token": "e7a7f2e5-181b-4caa-9142-592ab6787871"}
-post_data = json.dumps(data)
-header_dict = {'Content-Type': 'application/json'}
+    data = {"token": "e7a7f2e5-181b-4caa-9142-592ab6787871"}
+    post_data = json.dumps(data)
+    header_dict = {'Content-Type': 'application/json'}
 
-req = urllib.request.Request(url, data=bytes(post_data, 'gbk'), headers=header_dict)
+    req = urllib.request.Request(url, data=bytes(post_data, 'gbk'), headers=header_dict)
 
-res = urllib.request.urlopen(req).read().decode()
-print(res)
+    res = urllib.request.urlopen(req).read().decode()
 
-with open('../basicData/BasicData.pkl', 'wb') as pk_f:
-    pickle.dump(res, pk_f)
+    dict0 = json.loads(res)
+    res = json.dumps(dict0, indent=4, ensure_ascii=False)
+
+    # print(res)
+    with open("../basicData/res_basicData.txt", "w", encoding='utf-8') as f:
+        f.write(res)
