ae62e46bf2265e797e5a93ad5a39fa722445a6a9
李继隆 <delong1q2w3e@163.com>
2021-11-11 16:49:48 +0800

    添加 showPix.py

---
+++ gui/showPix.py
@@ -0,0 +1,35 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import sys
+import pandas as pd
+import numpy as np
+
+
+class ShowPix(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent=parent)
+
+        self.label = QLabel()
+        # self.pix = QPixmap(300, 200)
+        # color = QColor(40, 40, 40, 255)
+        # self.pix.fill(color)
+        #
+        self.pix = parent.data_pix.pix
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle('QTableViewDemo')
+        self.label.setPixmap(self.pix)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.label)
+
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = ShowPix()
+    main.show()
+    sys.exit(app.exec_())
