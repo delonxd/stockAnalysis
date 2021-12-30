5714cee5df1442be468106da5707cf4c5237890d
李继隆 <delong1q2w3e@163.com>
2021-12-30 15:55:43 +0800

    添加 test_discount.py

--- test/test_discount.py
+++ test/test_discount.py
@@ -1,5 +1,48 @@
 import numpy as np
 
+
+class ValueModel:
+    def __init__(self, pe, rate, year):
+        self.code = ''
+        self.name = ''
+        self.rate0 = -10
+        self.pe = pe
+        self.rate = rate
+        self.year = year
+
+    @property
+    def value(self):
+        x, y = cal_value2(
+            rate0=self.rate0,
+            rate1=self.rate[0],
+            n1=self.year[0],
+            rate2=self.rate[1],
+            n2=self.year[1],
+        )
+
+        return x
+
+    def value_k(self, p1, p2):
+        x1 = self.value
+        x2 = self.pe
+
+        print("Val: {}".format(x1))
+        print("PE: {}".format(x2))
+
+        k = x1 / x2
+
+        max_val = -np.inf
+        max_a = None
+        for a in np.arange(0, 1, 0.01):
+            ret = (p1 * np.log((k - 1) * a + 1) + p2 * np.log(1 - a)) / np.log(2)
+
+            if ret > max_val:
+                max_val = ret
+                max_a = a
+        print("Max_Val: {:.2f}".format(max_val))
+        print("Max_Percent: {:.0f}%".format(max_a * 100))
+
+
 def cal_value(a, n):
     res = (a ** n - 1) / (a - 1)
     return res
@@ -28,28 +71,27 @@ def cal_value2(rate0, rate1=0, rate2=0, n1=0, n2=0):
     return res, d_percent
 
 
+def cal_kelly(p1, p2, k):
+    max_val = -np.inf
+    max_a = None
+    for a in np.arange(0, 1, 0.01):
+        # print(a)
+        ret = (p1 * np.log((k-1)*a + 1) + p2 * np.log(1 - a)) / np.log(2)
+
+        if ret > max_val:
+            max_val = ret
+            max_a = a
+    print("Max_Val: {:.2f}".format(max_val))
+    print("Max_Percent: {:.0f}%".format(max_a * 100))
+
+
 if __name__ == '__main__':
-
-    val, d = cal_value2(
-        rate0=-10,
-
-        rate1=30,
-        n1=3,
-
-        rate2=0,
-        n2=100,
+    m = ValueModel(
+        pe=20,
+        rate=[5, 5],
+        year=[5, 30],
     )
-
-    print(val)
-    print("Discount: {:.2f}%".format(d))
-
-    pe = 100
-
-    log2_pe = np.log(pe) / np.log(2)
-    log2_val = np.log(val) / np.log(2) - log2_pe
-    print("PE: {}".format(pe))
-    print("Log2 PE: {:.2f}".format(log2_pe))
-    print("Res: {:.2f}".format(log2_val))
-
-
-
+    m.value_k(
+        p1=0.5,
+        p2=0.1,
+    )
