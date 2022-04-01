ae98ec04778b1a504f430a5ccc4ef80408195508
李继隆 <delong1q2w3e@163.com>
2022-04-01 11:44:24 +0800

    更新

--- sql/load_data_infile.py
+++ sql/load_data_infile.py
@@ -1,16 +1,21 @@
-import mysql.connector
-import os
-from request.requestData import get_header_df
-from method.sqlMethod import sql_format_create_table
-from method.sqlMethod import sql_format_drop_table
-from method.sqlMethod import sql_format_header_df
 
+def output_database(database, target_dir):
+    import mysql.connector
+    import time
+    import os
+    import shutil
 
-def test_output_sql():
+    date = time.strftime("%Y%m%d", time.localtime(time.time()))
+    dir_path = '%s/%s_backups_%s' % (target_dir, database, date)
+
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
     host = 'localhost'
     port = '3306'
-    # database = 'marketData'
-    database = 'fsData'
 
     config = {
         'user': 'root',
@@ -19,9 +24,6 @@ def test_output_sql():
         'port': port,
         'database': database,
     }
-
-    db_name = '%s_%s\\%s' % (host, port, database)
-    # MainLog.add_log('connect database --> ' + db_name)
 
     db = mysql.connector.connect(**config)
     cursor = db.cursor()
@@ -30,27 +32,28 @@ def test_output_sql():
     cursor.execute(sql_str)
     tmp_res = cursor.fetchall()
 
-    print(tmp_res)
-    # print(type(tmp_res))
-
-    # dir_path = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/mvs_20220126/"
-    dir_path = "F:/MysqlUploads/fs_backups_20220126/"
+    print('All table: ', tmp_res)
 
     for index in tmp_res:
         table = index[0]
-        print(table)
-        file = "%s%s_backups.sql" % (dir_path, table)
+        file = "%s/%s_backups.sql" % (dir_path, table)
         sql_str = 'SELECT * FROM %s INTO OUTFILE "%s";' % (table, file)
-        print(sql_str)
+        print(time.strftime("%Y-%m-%d %H:%M:%S  ", time.localtime(time.time())), sql_str)
         cursor.execute(sql_str)
         db.commit()
 
 
-def load_data_infile():
+def input_database(dir_path, database):
+    import mysql.connector
+    import time
+    import os
+    from request.requestData import get_header_df
+    from method.sqlMethod import sql_format_create_table
+    from method.sqlMethod import sql_format_drop_table
+    from method.sqlMethod import sql_format_header_df
+
     host = 'localhost'
     port = '3306'
-    # database = 'marketdata'
-    database = 'testdata'
 
     config = {
         'user': 'root',
@@ -60,29 +63,23 @@ def load_data_infile():
         'database': database,
     }
 
-    db_name = '%s_%s\\%s' % (host, port, database)
-    # MainLog.add_log('connect database --> ' + db_name)
-
     db = mysql.connector.connect(**config)
     cursor = db.cursor()
 
-    dir_path = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/mvs_backups/"
+    file_list = [x for x in os.listdir(dir_path) if os.path.isfile(dir_path + x)]
+    length = len(file_list)
 
-    list0 = [x for x in os.listdir(dir_path) if os.path.isfile(dir_path + x)]
-
-    length = len(list0)
-    a = list0.index('mvs_600007_backups.sql')
-
-    # print(a)
-    index = a
+    index = 0
     while index < length:
-        file = list0[index]
-        header_df = get_header_df('mvs')
+        file = file_list[index]
+        tmp = file.split('_')
+        table = '%s_%s' % (tmp[0], tmp[1])
+
+        header_df = get_header_df(tmp[0])
         header_str = sql_format_header_df(header_df)
 
-        table = file[:10]
         path = dir_path + file
-        print(path, table)
+        print(time.strftime("%Y-%m-%d %H:%M:%S  ", time.localtime(time.time())), path, table)
 
         cursor.execute(sql_format_drop_table(table, if_exists=True))
         db.commit()
@@ -96,9 +93,26 @@ def load_data_infile():
 
         index += 1
 
-        # break
+
+def output_databases():
+    # target_dir = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads"
+    target_dir = "F:/MysqlUploads"
+
+    output_database('fsData', target_dir)
+    output_database('marketData', target_dir)
+
+
+def input_databases():
+    src_dir = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads"
+    # src_dir = "F:/MysqlUploads"
+
+    dir_path = src_dir + '/fsData_backups_20220401/'
+    input_database(dir_path, 'testData')
+
+    dir_path = src_dir + '/marketData_backups_20220401/'
+    input_database(dir_path, 'testData')
 
 
 if __name__ == '__main__':
-    test_output_sql()
-    # load_data_infile()
+    output_databases()
+    # input_databases()
