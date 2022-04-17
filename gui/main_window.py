25cfc278c2676d6d170425e15da9032944987f54
Delon <delong1q2w3e@163.com>
2022-04-17 22:58:29 +0800

    更新

--- gui/main_window.py
+++ gui/main_window.py
@@ -91,7 +91,7 @@ class MainWidget(QWidget):
 
         self.codes_df = CodesDataFrame(code_list)
         # self.codes_df.init_current_index(index=1100)
-        self.codes_df.init_current_index(index=64)
+        self.codes_df.init_current_index(index=0)
         # self.codes_df.init_current_index(code='688261')
         # self.codes_df.init_current_index(code='002260')
 
