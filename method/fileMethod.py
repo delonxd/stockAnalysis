3de4100c5e7fd446aac89e5bfdd3811594ed1d6e
李继隆 <delong1q2w3e@163.com>
2022-05-07 15:17:35 +0800

    更新

--- method/fileMethod.py
+++ method/fileMethod.py
@@ -6,7 +6,7 @@ from method.logMethod import MainLog
 def dump_pkl(path, data):
     with open(path, "wb") as f:
         pickle.dump(data, f)
-    MainLog.add_log('write pkl --> %s' % path)
+    MainLog.add_log('dump pkl --> %s' % path)
 
 
 def load_pkl(path):
@@ -26,5 +26,5 @@ def write_json_txt(path, data):
 def load_json_txt(path):
     with open(path, "r", encoding="utf-8", errors="ignore") as f:
         ret = json.loads(f.read())
-    MainLog.add_log('load json txt --> %s' % path)
+    MainLog.add_log('loads json txt --> %s' % path)
     return ret
