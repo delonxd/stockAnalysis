914d715433f062e13d1ea170822bc253edc11a52
Delon <delong1q2w3e@163.com>
2023-11-01 15:06:22 +0800

    更新 urlMethod.py 添加 ProxyHandler

--- method/urlMethod.py
+++ method/urlMethod.py
@@ -8,7 +8,11 @@ def data_request(url, api_dict):
     header_dict = {'Content-Type': 'application/json'}
 
     req = urllib.request.Request(url, data=bytes(api, 'gbk'), headers=header_dict)
-    res = urllib.request.urlopen(req).read()
+    # res = urllib.request.urlopen(req).read()
+
+    handler = urllib.request.ProxyHandler(proxies={})
+    opener = urllib.request.build_opener(handler)
+    res = opener.open(req).read()
 
     return res
 
