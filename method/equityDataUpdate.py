489f18a866a12f9242e973b49583d2d6333fce42
Delon <delong1q2w3e@163.com>
2024-01-07 20:09:16 +0800

    更新 equityDataUpdate.py 添加 更新期货信息 request_futures_data() 添加 更新仓位 sort_hold()

--- method/equityDataUpdate.py
+++ method/equityDataUpdate.py
@@ -1,4 +1,9 @@
 def eq_update():
+    import warnings
+    from scipy.optimize import OptimizeWarning
+    warnings.simplefilter("ignore", OptimizeWarning)
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+
     import sys
     sys.path.append('D:\\PycharmProjects\\stockAnalysis')
 
@@ -10,11 +15,22 @@ def eq_update():
     from request.requestEquityData import request_eq2mysql
     from request.requestDividendData import request_dv2mysql
 
+    from request.requestAkshareData import request_mir_y10_ak
+    from request.requestAkshareData import request_futures_data
+
+    from method.sortCode import sort_hold
+
     code_list = load_json_txt("..\\basicData\\dailyUpdate\\latest\\a001_code_list.txt")
     request_eq2mysql(code_list)
     request_dv2mysql(code_list)
 
-    MainLog.write('..\\basicData\\dailyUpdate\\eq_update_log.txt', init=True)
+    MainLog.write('..\\basicData\\dailyUpdate\\eq_update_log1.txt', init=True)
+
+    request_mir_y10_ak()
+    request_futures_data()
+    sort_hold()
+
+    MainLog.write('..\\basicData\\dailyUpdate\\eq_update_log2.txt', init=True)
 
 
 if __name__ == '__main__':
