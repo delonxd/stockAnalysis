3e33ea346aa92d6d1a1d921f63e85859c53e10f1
Delon <delong1q2w3e@163.com>
2024-07-07 20:45:02 +0800

    更新 requestEquityData.py 重写 config_eq_res() 添加 初始化ini参数

--- request/requestEquityData.py
+++ request/requestEquityData.py
@@ -1,5 +1,6 @@
 from request.requestData import *
 from method.fileMethod import *
+from method.sqlMethod import sql_format_drop_table
 
 import datetime as dt
 import json
@@ -62,6 +63,45 @@ def config_eq_res(data):
 
     # func = lambda x: '-' if x == 0 else x
 
+    # ipo_rate = 1
+    # for row in res:
+    #     if last:
+    #         d1 = row[1] - last[1]
+    #         d2 = row[2] - last[2]
+    #         d3 = row[3] - last[3]
+    #         d4 = row[4] - last[4]
+    #
+    #         # if d1 == d2 == d3 == d4 == 0 and row[5] == '定期报告':
+    #         if d1 == d2 == d3 == d4 == 0 and row[5] == 'periodicReport':
+    #             new = [*row, d1, d2, d3, d4, last[10], 1]
+    #             res2.append(new)
+    #
+    #         else:
+    #             rate = row[1] / last[1]
+    #             # if row[5] == '送、转股' or row[5] == '拆细':
+    #             if row[5] == 'dividend' or row[5] == 'split':
+    #                 # if abs(row[2] - last[2] * rate) <= 50:
+    #                 #     if abs(row[3] - last[3] * rate) <= 50:
+    #                 #         if abs(row[4] - last[4] * rate) <= 50:
+    #                 rate = 1.0
+    #
+    #             tmp = last[10]*rate
+    #             if row[5] == 'IPO':
+    #                 ipo_rate = tmp
+    #             # new = [*row, func(d1), func(d2), func(d3), func(d4), tmp, round(rate, 4)]
+    #             new = [*row, d1, d2, d3, d4, tmp, round(rate, 4)]
+    #             res2.append(new)
+    #     else:
+    #         d1 = row[1]
+    #         d2 = row[2]
+    #         d3 = row[3]
+    #         d4 = row[4]
+    #
+    #         # new = [*row, func(d1), func(d2), func(d3), func(d4), 1, 1]
+    #         new = [*row, d1, d2, d3, d4, 1, 1]
+    #         res2.append(new)
+    #     last = res2[-1]
+
     ipo_rate = 1
     for row in res:
         if last:
@@ -70,33 +110,30 @@ def config_eq_res(data):
             d3 = row[3] - last[3]
             d4 = row[4] - last[4]
 
-            # if d1 == d2 == d3 == d4 == 0 and row[5] == '定期报告':
-            if d1 == d2 == d3 == d4 == 0 and row[5] == 'periodicReport':
-                new = [*row, d1, d2, d3, d4, last[10], 1]
-                res2.append(new)
+            rate = row[1] / last[1]
+            if row[5] == 'dividend' or row[5] == 'split':
+                rate = 1.0
 
+            tmp = last[10]*rate
+            if row[5] == 'IPO':
+                ipo_rate = tmp
+
+            new = [*row, d1, d2, d3, d4, tmp, round(rate, 4)]
+            if new[0] == last[0]:
+                d1 = last[6] + new[6]
+                d2 = last[7] + new[7]
+                d3 = last[8] + new[8]
+                d4 = last[9] + new[9]
+                r1 = new[10]
+                r2 = round(last[11] * new[11], 4)
+                res2[-1] = [*row, d1, d2, d3, d4, r1, r2]
             else:
-                rate = row[1] / last[1]
-                # if row[5] == '送、转股' or row[5] == '拆细':
-                if row[5] == 'dividend' or row[5] == 'split':
-                    # if abs(row[2] - last[2] * rate) <= 50:
-                    #     if abs(row[3] - last[3] * rate) <= 50:
-                    #         if abs(row[4] - last[4] * rate) <= 50:
-                    rate = 1.0
-
-                tmp = last[10]*rate
-                if row[5] == 'IPO':
-                    ipo_rate = tmp
-                # new = [*row, func(d1), func(d2), func(d3), func(d4), tmp, round(rate, 4)]
-                new = [*row, d1, d2, d3, d4, tmp, round(rate, 4)]
                 res2.append(new)
         else:
             d1 = row[1]
             d2 = row[2]
             d3 = row[3]
             d4 = row[4]
-
-            # new = [*row, func(d1), func(d2), func(d3), func(d4), 1, 1]
             new = [*row, d1, d2, d3, d4, 1, 1]
             res2.append(new)
         last = res2[-1]
@@ -118,14 +155,17 @@ def config_eq_res(data):
     return res_df, check_field, header_df
 
 
-def eq_res2mysql(res, code):
-    db, cursor = get_cursor('eq')
+def eq_res2mysql(res, code, ini=False):
 
     df, check_field, header_df = config_eq_res(res)
+    db, cursor = get_cursor('eq')
 
     table = '%s_%s' % ('eq', code)
 
     MainLog.add_log('    table --> %s' % table)
+
+    if ini is True:
+        cursor.execute(sql_format_drop_table(table))
 
     header_str = sql_format_header_df(header_df)
     cursor.execute(sql_format_create_table(table, header_str))
@@ -136,25 +176,25 @@ def eq_res2mysql(res, code):
         table=table,
         df_data=df,
         check_field=check_field,
-        # ini=True,
-        ini=False,
+        ini=ini,
+        # ini=False,
     )
 
-    if len(new_data.index) == 0:
-        MainLog.add_log('    new data: None')
-        return
-    else:
-        MainLog.add_log('    new data:\n%s' % repr(new_data))
+    # if len(new_data.index) == 0:
+    #     MainLog.add_log('    new data: None')
+    #     return
+    # else:
+    #     MainLog.add_log('    new data:\n%s' % repr(new_data))
 
     db.close()
 
     return new_data
 
 
-def request_eq2mysql(stock_codes):
+def request_eq2mysql(stock_codes, ini=True):
     for code in stock_codes:
         res = request_equity_change(code)
-        eq_res2mysql(res, code)
+        eq_res2mysql(res, code, ini)
         MainLog.add_split('-')
 
 
@@ -163,8 +203,10 @@ if __name__ == '__main__':
     pd.set_option('display.max_rows', None)
     pd.set_option('display.width', 10000)
 
-    list1 = load_json_txt("..\\basicData\\self_selected\\gui_whitelist.txt")
-    list2 = load_json_txt("..\\basicData\\dailyUpdate\\latest\\s004_code_latest_update.txt")
-    list3 = list(set(list1 + list2))
-    request_eq2mysql(list3)
+    # list1 = load_json_txt("..\\basicData\\self_selected\\gui_whitelist.txt")
+    # list2 = load_json_txt("..\\basicData\\dailyUpdate\\latest\\s004_code_latest_update.txt")
+    # list3 = list(set(list1 + list2))
+    # request_eq2mysql(list3)
+    code_list = load_json_txt("..\\basicData\\dailyUpdate\\latest\\a001_code_list.txt")
+    request_eq2mysql(code_list)
     pass
