1a3c5cf7c6f81279e1939dc9e7a1f7b137f2f2fe
Delon <delong1q2w3e@163.com>
2024-08-20 12:00:54 +0800

    更新 sql_update.py 注释不用的函数

--- method/sql_update.py
+++ method/sql_update.py
@@ -92,103 +92,103 @@ def update_all_data(code_list, start_date, fs_flag=True, mvs_flag=True):
     return ret
 
 
-def tmp_update():
-    from request.requestData import request2mysql
-    from request.requestData import request2mysql_daily
-    from request.requestEquityData import request_eq2mysql
-
-    # path = "..\\basicData\\dailyUpdate\\latest\\s004_code_latest_update.txt"
-    # path = "..\\basicData\\analyzedData\\temp_codes.txt"
-    path = "..\\basicData\\dailyUpdate\\latest\\a001_code_list.txt"
-    with open(path, "r", encoding="utf-8", errors="ignore") as f:
-        code_list = json.loads(f.read())
-
-    request_eq2mysql(code_list)
-    # code_list = list(filter(lambda x: x < '100000', code_list))
-    # code_list = code_list[:1]
-    # code_list = ['000002']
-    # code_list.reverse()
-    # for code in code_list:
-    #     print(code)
-    #     # break
-    #     request2mysql(
-    #         stock_code=code,
-    #         data_type='mvs',
-    #         # start_date='2015-01-01',
-    #         start_date='1970-01-01',
-    #         # end_date='2022-01-01',
-    #         metrics=['ta'],
-    #     )
-    #     request2mysql()
-
-    # request2mysql_daily(
-    #     stock_codes=code_list,
-    #     data_type='mvs',
-    #     date='latest',
-    #     # metrics=['q.ps.foe_r.t'],
-    #     # metrics=['q.bs.ta.t'],
-    # )
-
-    # update_all_data(code_list, start_date='2021-01-01')
-
-
-def update_header():
-    from method.sqlMethod import sql_drop_field, sql_add_field
-    from request.requestData import get_cursor
-
-    data_type = 'mvs'
-    database = 'marketData'
-    db, cursor = get_cursor(data_type)
-
-    sql_str = 'SELECT table_name FROM information_schema.TABLES WHERE table_schema = "%s";' % database
-    cursor.execute(sql_str)
-    tmp_res = cursor.fetchall()
-
-    MainLog.add_log('All table: %s' % tmp_res)
-
-    for index in tmp_res:
-        table = index[0]
-        field = 'id_048_mvs_ta'
-        command = sql_add_field(table, field, 'DOUBLE')
-        # print(command)
-        cursor.execute(command)
-        db.commit()
-        MainLog.add_log('Table %s add field %s' % (table, field))
-
-
-def test_compare():
-    from method.dataMethod import load_df_from_mysql
-    import numpy as np
-
-    data_type = 'mvs'
-    code = '000002'
-
-    df1 = load_df_from_mysql(code, data_type)
-    df2 = load_df_from_mysql(code+'_copy1', data_type)
-
-    df1 = df1.drop(['first_update', 'last_update'], axis=1)
-    df2 = df2.drop(['first_update', 'last_update'], axis=1)
-
-    for column in df1.columns:
-        for index in df1.index:
-            try:
-                val1 = df1.loc[index, column]
-                val2 = df2.loc[index, column]
-
-                if val1 == val2:
-                    pass
-                elif np.isnan(val1) and np.isnan(val2):
-                    pass
-                else:
-                    print(index, column, val1, val2)
-
-            except:
-                continue
-    # print(df1.columns)
+# def tmp_update():
+#     from request.requestData import request2mysql
+#     from request.requestData import request2mysql_daily
+#     from request.requestEquityData import request_eq2mysql
+#
+#     # path = "..\\basicData\\dailyUpdate\\latest\\s004_code_latest_update.txt"
+#     # path = "..\\basicData\\analyzedData\\temp_codes.txt"
+#     path = "..\\basicData\\dailyUpdate\\latest\\a001_code_list.txt"
+#     with open(path, "r", encoding="utf-8", errors="ignore") as f:
+#         code_list = json.loads(f.read())
+#
+#     request_eq2mysql(code_list)
+#     # code_list = list(filter(lambda x: x < '100000', code_list))
+#     # code_list = code_list[:1]
+#     # code_list = ['000002']
+#     # code_list.reverse()
+#     # for code in code_list:
+#     #     print(code)
+#     #     # break
+#     #     request2mysql(
+#     #         stock_code=code,
+#     #         data_type='mvs',
+#     #         # start_date='2015-01-01',
+#     #         start_date='1970-01-01',
+#     #         # end_date='2022-01-01',
+#     #         metrics=['ta'],
+#     #     )
+#     #     request2mysql()
+#
+#     # request2mysql_daily(
+#     #     stock_codes=code_list,
+#     #     data_type='mvs',
+#     #     date='latest',
+#     #     # metrics=['q.ps.foe_r.t'],
+#     #     # metrics=['q.bs.ta.t'],
+#     # )
+#
+#     # update_all_data(code_list, start_date='2021-01-01')
+#
+#
+# def update_header():
+#     from method.sqlMethod import sql_drop_field, sql_add_field
+#     from request.requestData import get_cursor
+#
+#     data_type = 'mvs'
+#     database = 'marketData'
+#     db, cursor = get_cursor(data_type)
+#
+#     sql_str = 'SELECT table_name FROM information_schema.TABLES WHERE table_schema = "%s";' % database
+#     cursor.execute(sql_str)
+#     tmp_res = cursor.fetchall()
+#
+#     MainLog.add_log('All table: %s' % tmp_res)
+#
+#     for index in tmp_res:
+#         table = index[0]
+#         field = 'id_048_mvs_ta'
+#         command = sql_add_field(table, field, 'DOUBLE')
+#         # print(command)
+#         cursor.execute(command)
+#         db.commit()
+#         MainLog.add_log('Table %s add field %s' % (table, field))
+#
+#
+# def test_compare():
+#     from method.dataMethod import load_df_from_mysql
+#     import numpy as np
+#
+#     data_type = 'mvs'
+#     code = '000002'
+#
+#     df1 = load_df_from_mysql(code, data_type)
+#     df2 = load_df_from_mysql(code+'_copy1', data_type)
+#
+#     df1 = df1.drop(['first_update', 'last_update'], axis=1)
+#     df2 = df2.drop(['first_update', 'last_update'], axis=1)
+#
+#     for column in df1.columns:
+#         for index in df1.index:
+#             try:
+#                 val1 = df1.loc[index, column]
+#                 val2 = df2.loc[index, column]
+#
+#                 if val1 == val2:
+#                     pass
+#                 elif np.isnan(val1) and np.isnan(val2):
+#                     pass
+#                 else:
+#                     print(index, column, val1, val2)
+#
+#             except:
+#                 continue
+#     # print(df1.columns)
 
 
 if __name__ == '__main__':
-    import json
+    # import json
     import pandas as pd
 
     pd.set_option('display.max_columns', None)
@@ -198,6 +198,5 @@ if __name__ == '__main__':
     # mysql_update()
     # mysql_update_daily()
     # update_latest_data2()
-    tmp_update()
+    # tmp_update()
     # test_compare()
-
