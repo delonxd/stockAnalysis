feb1d3df2f4da186d707bbc96f6629c643d7cb1f
Delon <delong1q2w3e@163.com>
2025-03-06 20:05:39 +0800

    更新 remarkWidget.py 添加了gui_ud的功能

--- gui/remarkWidget.py
+++ gui/remarkWidget.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from method.fileMethod import *
+from method.mainMethod import value2credit_rating, credit_rating2value
 import sys
 
 
@@ -17,38 +18,56 @@ class RemarkWidget(QWidget):
         self.main_widget = main_widget
 
         self.label = QLabel('000000: 测试')
-        self.label1 = QLabel('  复合增长率：')
-        self.label2 = QLabel('%     估值：')
-        self.label3 = QLabel('亿')
-        self.label4 = QLabel('境外占比：')
-        self.label5 = QLabel('% ')
+        self.label11 = QLabel('  复合增长率：')
+        self.label12 = QLabel('%     估值：')
+        self.label13 = QLabel('亿')
+        self.label21 = QLabel('  业务了解度：')
+        self.label22 = QLabel('境外占比：')
+        self.label23 = QLabel('% ')
+        self.label31 = QLabel('  盈利长期性：')
+        self.label32 = QLabel('管理层信用：')
+        self.label33 = QLabel('')
 
         self.editor0 = QLineEdit()
         self.editor1 = QLineEdit()
         self.editor2 = QTextEdit()
         self.editor3 = QLineEdit()
         self.editor4 = QLineEdit()
+        self.editor5 = QLineEdit()
+        self.editor6 = QLineEdit()
+        self.editor7 = QLineEdit()
         self.button1 = QPushButton('上传备注')
         self.button2 = QPushButton('下载备注')
 
         self.label.setFont(QFont('Consolas', 18))
-        self.label1.setFont(QFont('Consolas', 15))
-        self.label2.setFont(QFont('Consolas', 15))
-        self.label3.setFont(QFont('Consolas', 15))
-        self.label4.setFont(QFont('Consolas', 15))
-        self.label5.setFont(QFont('Consolas', 15))
+        self.label11.setFont(QFont('Consolas', 15))
+        self.label12.setFont(QFont('Consolas', 15))
+        self.label13.setFont(QFont('Consolas', 15))
+        self.label21.setFont(QFont('Consolas', 15))
+        self.label22.setFont(QFont('Consolas', 15))
+        self.label23.setFont(QFont('Consolas', 15))
+        self.label31.setFont(QFont('Consolas', 15))
+        self.label32.setFont(QFont('Consolas', 15))
+        self.label33.setFont(QFont('Consolas', 15))
 
         self.editor0.setFont(QFont('Consolas', 15))
         self.editor1.setFont(QFont('Consolas', 18))
         self.editor2.setFont(QFont('Consolas', 18))
         self.editor3.setFont(QFont('Consolas', 15))
         self.editor4.setFont(QFont('Consolas', 15))
+        self.editor5.setFont(QFont('Consolas', 15))
+        self.editor6.setFont(QFont('Consolas', 15))
+        self.editor7.setFont(QFont('Consolas', 15))
 
         self.editor0.setAlignment(Qt.AlignmentFlag.AlignRight)
         self.editor3.setAlignment(Qt.AlignmentFlag.AlignRight)
         self.editor4.setAlignment(Qt.AlignmentFlag.AlignRight)
-        self.label2.setAlignment(Qt.AlignmentFlag.AlignRight)
-        self.label4.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.editor5.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.editor6.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.editor7.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.label12.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.label22.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.label32.setAlignment(Qt.AlignmentFlag.AlignRight)
 
         validator = QIntValidator(self)
         validator.setRange(1, 1000000)
@@ -68,14 +87,21 @@ class RemarkWidget(QWidget):
         layout = QVBoxLayout()
 
         layout1 = QGridLayout()
-        layout1.addWidget(self.label1, 0, 0)
+        layout1.addWidget(self.label11, 0, 0)
         layout1.addWidget(self.editor3, 0, 1)
-        layout1.addWidget(self.label2, 0, 2)
+        layout1.addWidget(self.label12, 0, 2)
         layout1.addWidget(self.editor0, 0, 3)
-        layout1.addWidget(self.label3, 0, 4)
-        layout1.addWidget(self.label4, 1, 2)
+        layout1.addWidget(self.label13, 0, 4)
+        layout1.addWidget(self.label21, 1, 0)
+        layout1.addWidget(self.editor5, 1, 1)
+        layout1.addWidget(self.label22, 1, 2)
         layout1.addWidget(self.editor4, 1, 3)
-        layout1.addWidget(self.label5, 1, 4)
+        layout1.addWidget(self.label23, 1, 4)
+        layout1.addWidget(self.label31, 2, 0)
+        layout1.addWidget(self.editor6, 2, 1)
+        layout1.addWidget(self.label32, 2, 2)
+        layout1.addWidget(self.editor7, 2, 3)
+        layout1.addWidget(self.label33, 2, 4)
 
         layout.addWidget(self.label)
         layout.addLayout(layout1)
@@ -95,6 +121,12 @@ class RemarkWidget(QWidget):
         self.write_content(self.editor3.text(), "gui_rate.txt")
         self.write_content(self.editor4.text(), "gui_export.txt")
 
+        val1 = self.editor5.text()
+        val2 = self.editor6.text()
+        val3 = self.editor7.text()
+        val = list(map(lambda x: credit_rating2value(x), [val1, val2, val3]))
+        self.write_content(val, "gui_ud.txt", non=[None, None, None])
+
         self.main_widget.show_stock_name()
 
     def load_tags(self):
@@ -104,12 +136,12 @@ class RemarkWidget(QWidget):
         txt = res[code] if code in res else ''
         self.editor1.setText(txt)
 
-    def write_content(self, val, file):
+    def write_content(self, val, file, non: object = ''):
         path = "../basicData/self_selected/%s" % file
         code = self.code
         res = load_json_txt(path, log=False)
         res[code] = val
-        if val == '':
+        if val == non:
             res.pop(code)
         write_json_txt(path, res, log=False)
 
@@ -125,6 +157,13 @@ class RemarkWidget(QWidget):
         path = "../basicData/self_selected/gui_tags.txt"
         res = load_json_txt(path, log=False)
         txt2 = res[code] if code in res else ''
+
+        path = "../basicData/self_selected/gui_ud.txt"
+        res = load_json_txt(path, log=False)
+
+        value3 = res.get(code)
+        value3 = [None] * 3 if value3 is None else value3
+        value3 = list(map(lambda x: value2credit_rating(x), value3))
 
         path = "../basicData/self_selected/gui_assessment.txt"
         res = load_json_txt(path, log=False)
@@ -144,6 +183,9 @@ class RemarkWidget(QWidget):
         self.editor0.setText(value)
         self.editor3.setText(value1)
         self.editor4.setText(value2)
+        self.editor5.setText(value3[0])
+        self.editor6.setText(value3[1])
+        self.editor7.setText(value3[2])
 
         # print(self.textEdit.toHtml())
 
