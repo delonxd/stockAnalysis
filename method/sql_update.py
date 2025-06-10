2b8586144871700ad306fcebabafb2466d8c5b5f
Delon <delong1q2w3e@163.com>
2025-06-10 22:08:52 +0800

    更新 sql_update.py 添加新的更新cn数据的方法 添加 update_latest_data_cn() 添加 update_all_data_cn()

--- method/sql_update.py
+++ method/sql_update.py
@@ -1,96 +1,99 @@
 from method.logMethod import MainLog
+from request.requestData import request2mysql_daily_new, request2mysql_new
+from method.mainMethod import get_code_type
+from method.fileMethod import load_json_txt
 
 
-def update_latest_data(code_list, fs_flag=True, mvs_flag=True):
-    from request.requestData import request2mysql_daily
-
-    list0 = []
-    counter = 0
-    for code in code_list:
-        if counter == 0:
-            list0.append([])
-            counter = 100
-        list0[-1].append(code)
-        counter -= 1
-
-    length = len(list0)
-    MainLog.add_split('#')
-    MainLog.add_log('total %s groups' % length)
-    # print(length)
-    # print(list0)
-
-    start = 0
-    end = length
-
-    ret = []
-
-    index = start
-    while index < end:
-        stock_codes = list0[index]
-
-        MainLog.add_split('-')
-        MainLog.add_log('group %s/%s' % (index, end))
-        MainLog.add_split('-')
-
-        if fs_flag is True:
-            new_data = request2mysql_daily(
-                stock_codes=stock_codes,
-                date='latest',
-                data_type='fs',
-            )
-
-            ret.extend(new_data)
-
-        if mvs_flag is True:
-            MainLog.add_split('-')
-            request2mysql_daily(
-                stock_codes=stock_codes,
-                date='latest',
-                data_type='mvs',
-            )
-
-        index += 1
-
-    return ret
-
-
-def update_all_data(code_list, start_date, fs_flag=True, mvs_flag=True):
-    from request.requestData import request2mysql
-
-    index = 0
-    end = len(code_list)
-
-    ret = []
-
-    while index < end:
-        code = code_list[index]
-
-        MainLog.add_split('#')
-        MainLog.add_log('%s/%s --> %s' % (index, end, code))
-
-        if fs_flag is True:
-            new_data = request2mysql(
-                stock_code=code,
-                data_type='fs',
-                start_date=start_date,
-            )
-
-            if new_data is not None:
-                ret.append(code)
-
-            MainLog.add_split('-')
-
-        if mvs_flag is True:
-            request2mysql(
-                stock_code=code,
-                data_type='mvs',
-                start_date=start_date,
-            )
-
-        index += 1
-
-    return ret
-
+# def update_latest_data(code_list, fs_flag=True, mvs_flag=True):
+#     from request.requestData import request2mysql_daily
+#
+#     list0 = []
+#     counter = 0
+#     for code in code_list:
+#         if counter == 0:
+#             list0.append([])
+#             counter = 100
+#         list0[-1].append(code)
+#         counter -= 1
+#
+#     length = len(list0)
+#     MainLog.add_split('#')
+#     MainLog.add_log('total %s groups' % length)
+#     # print(length)
+#     # print(list0)
+#
+#     start = 0
+#     end = length
+#
+#     ret = []
+#
+#     index = start
+#     while index < end:
+#         stock_codes = list0[index]
+#
+#         MainLog.add_split('-')
+#         MainLog.add_log('group %s/%s' % (index, end))
+#         MainLog.add_split('-')
+#
+#         if fs_flag is True:
+#             new_data = request2mysql_daily(
+#                 stock_codes=stock_codes,
+#                 date='latest',
+#                 data_type='fs',
+#             )
+#
+#             ret.extend(new_data)
+#
+#         if mvs_flag is True:
+#             MainLog.add_split('-')
+#             request2mysql_daily(
+#                 stock_codes=stock_codes,
+#                 date='latest',
+#                 data_type='mvs',
+#             )
+#
+#         index += 1
+#
+#     return ret
+#
+#
+# def update_all_data(code_list, start_date, fs_flag=True, mvs_flag=True):
+#     from request.requestData import request2mysql
+#
+#     index = 0
+#     end = len(code_list)
+#
+#     ret = []
+#
+#     while index < end:
+#         code = code_list[index]
+#
+#         MainLog.add_split('#')
+#         MainLog.add_log('%s/%s --> %s' % (index, end, code))
+#
+#         if fs_flag is True:
+#             new_data = request2mysql(
+#                 stock_code=code,
+#                 data_type='fs',
+#                 start_date=start_date,
+#             )
+#
+#             if new_data is not None:
+#                 ret.append(code)
+#
+#             MainLog.add_split('-')
+#
+#         if mvs_flag is True:
+#             request2mysql(
+#                 stock_code=code,
+#                 data_type='mvs',
+#                 start_date=start_date,
+#             )
+#
+#         index += 1
+#
+#     return ret
+#
 
 # def tmp_update():
 #     from request.requestData import request2mysql
@@ -187,6 +190,128 @@ def update_all_data(code_list, start_date, fs_flag=True, mvs_flag=True):
 #     # print(df1.columns)
 
 
+def split_cn_code_by_type(code_list: list) -> dict:
+    path = '..\\basicData\\code_types_dict.txt'
+    type_dict = load_json_txt(path, log=False)
+
+    ret = {'bank': []}
+    for code in code_list:
+        code_type = get_code_type(code, type_dict=type_dict)
+
+        sub_list = ret.get(code_type)
+        if sub_list is None:
+            sub_list = []
+
+        sub_list.append(code)
+        ret[code_type] = sub_list
+    return ret
+
+
+def update_latest_data_cn(code_list, fs_flag=False, mvs_flag=False):
+    sub_table = split_cn_code_by_type(code_list)
+
+    ret = []
+    for fs_type, sub_list in sub_table.items():
+
+        list0 = []
+        counter = 0
+        for code in sub_list:
+            if counter == 0:
+                list0.append([])
+                counter = 100
+            list0[-1].append(code)
+            counter -= 1
+
+        length = len(list0)
+        MainLog.add_split('#')
+        MainLog.add_log('total %s groups' % length)
+
+        start = 0
+        end = length
+
+        ret = []
+
+        index = start
+        while index < end:
+            stock_codes = list0[index]
+
+            MainLog.add_split('-')
+            MainLog.add_log('group %s/%s' % (index + 1, end))
+            MainLog.add_split('-')
+
+            if fs_flag is True:
+                new_data = request2mysql_daily_new(
+                    stock_codes=stock_codes,
+                    date='latest',
+                    data_type='fs',
+                    fs_type=fs_type,
+                )
+                ret.extend(new_data)
+
+            if mvs_flag is True:
+                MainLog.add_split('-')
+                request2mysql_daily_new(
+                    stock_codes=stock_codes,
+                    date='latest',
+                    data_type='mvs',
+                    fs_type=fs_type,
+                )
+
+            index += 1
+
+    return ret
+
+
+def update_all_data_cn(code_list, start_date, fs_flag=False, mvs_flag=False):
+    sub_table = split_cn_code_by_type(code_list)
+    ret = []
+
+    for fs_type, sub_list in sub_table.items():
+
+        index = 0
+        end = len(sub_list)
+
+        while index < end:
+            code = sub_list[index]
+
+            MainLog.add_split('#')
+            MainLog.add_log('%s/%s --> %s' % (index, end, code))
+
+            if fs_flag is True:
+                new_data = request2mysql_new(
+                    stock_code=code,
+                    start_date=start_date,
+                    data_type='fs',
+                    fs_type=fs_type,
+                )
+                if new_data is not None:
+                    ret.append(code)
+
+                MainLog.add_split('-')
+
+            if mvs_flag is True:
+                request2mysql_new(
+                    stock_code=code,
+                    start_date=start_date,
+                    data_type='mvs',
+                    fs_type=fs_type,
+                )
+
+            index += 1
+
+    return ret
+
+
+def test_update():
+    from method.fileMethod import load_json_txt
+    path = "..\\basicData\\dailyUpdate\\latest\\a001_code_list.txt"
+    all_codes = load_json_txt(path)
+
+    # update_latest_data_cn(all_codes, fs_flag=True)
+    # update_all_data_cn(all_codes, start_date='1970-01-01', mvs_flag=True)
+    update_all_data_cn(all_codes, start_date='1970-01-01', fs_flag=True)
+
+
 if __name__ == '__main__':
     # import json
     import pandas as pd
@@ -195,6 +320,7 @@ if __name__ == '__main__':
     pd.set_option('display.max_rows', 3)
     pd.set_option('display.width', 10000)
 
+    test_update()
     # mysql_update()
     # mysql_update_daily()
     # update_latest_data2()
